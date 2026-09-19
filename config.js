8cc00c7e8c7f7d0009ec499450ac1db3b3d9e8d4
Taourga <saibi.yanis@gmail.com>
2026-09-19 17:44:32 +0200

    Configure DZAIRLY Supabase backend

---
+++ config.js
@@ -0,0 +1,6 @@
+window.DZAIRLY_CONFIG = {
+  supabaseUrl: "https://atykrmpkoinbeqbyxfyo.supabase.co",
+  supabasePublishableKey: "sb_publishable_JAroxdK19nne_QsI0iiL1w_UlTZOYqO",
+  whatsappNumber: "",
+  contactEmail: ""
+};
